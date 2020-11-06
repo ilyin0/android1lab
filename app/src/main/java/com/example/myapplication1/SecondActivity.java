c5a998f5be7bce1ce6861033646e3862f28d34ee
ilyin0 <ilyindeveloper@gmail.com>
2020-11-06 17:04:00 +0300

    Delete second activity layout.

--- app/src/main/java/com/example/myapplication1/SecondActivity.java
+++ app/src/main/java/com/example/myapplication1/SecondActivity.java
@@ -14,20 +14,20 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form2);
+        setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
         int number = intent.getIntExtra("number", 0);
         int sourceNumber = intent.getIntExtra("sourceNumber", 0);
-        TextView textView = (TextView) findViewById(R.id.textView3);
+        TextView textView = (TextView) findViewById(R.id.textView2);
         int sum = number + sourceNumber;
         textView.setText(String.valueOf(sum));
     }
 
     public void putText(View view) {
         Intent intent = new Intent(this, MainActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        TextView textView = (TextView) findViewById(R.id.textView3);
+        EditText editText = (EditText) findViewById(R.id.input);
+        TextView textView = (TextView) findViewById(R.id.textView2);
         String stringNumber = editText.getText().toString();
         int number = stringNumber.isEmpty() ? 0 : Integer.parseInt(stringNumber);
         String stringSourceNumber = textView.getText().toString();
